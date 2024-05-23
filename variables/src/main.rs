efbe8d33df6e91953f6ac4e7e453eb06b57391ec
catzillaorz <catzillaorz@outlook.com>
2024-05-23 15:45:07 +0800

    feat: variables demo

---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut x = 5;
+    println!("Hello, world! -- {}", x) ;
+    x = 6;
+    println!("Hello, world! {}", x);
+    let x = x + 1;
+    println!("Hello, world! --  {}", x);
+
+
+}
