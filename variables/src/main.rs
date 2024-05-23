2c0437d78b04c05cf617e6d38266eab4c52433c4
catzillaorz <catzillaorz@outlook.com>
2024-05-23 16:20:26 +0800

    feat: variables tuple

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,8 +3,25 @@ fn main() {
     println!("Hello, world! -- {}", x) ;
     x = 6;
     println!("Hello, world! {}", x);
+
     let x = x + 1;
     println!("Hello, world! --  {}", x);
 
+    let x = 2.0;
+    println!("Hello, world! --  {}", x);
+
+    let x: f32 = 3.0;
+    println!("Hello, world! --  {}", x);
+
+    let x: bool  = false;
+    println!("Hello, world! --  {}", x);
+
+
+    let  tup  = (500, 6.4, 1);
+    let (_x,y,_z) = tup;
+    println!("Hello, world! --  {}", tup.0);
+    println!("Hello, world! --  {}", tup.1);
+    println!("Hello, world! --  {}", tup.2);
+    println!("Hello, world! --  {}", y);
 
 }
