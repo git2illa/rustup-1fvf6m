8317d3a4080df05fc72fe471d0a9daa129ebbc69
catzillaorz <catzillaorz@outlook.com>
2024-05-24 15:52:59 +0800

    feat: struct refactor

--- reactanle_area_calc/src/main.rs
+++ reactanle_area_calc/src/main.rs
@@ -1,8 +1,30 @@
+#[derive(Debug)]
+struct Reactangle {
+    width: u32,
+    height: u32,
+}
 fn main() {
     let square = (20,30);
+
     println!("This reactangle area is {} square pixels.", basic_calc_reactangle_area(square));
+
+
+    let square = Reactangle{
+        width: 20,
+        height: 50
+    };
+
+    println!("-- {}", struct_calc_area(&square));
 }
 
+// tuple way 
 fn basic_calc_reactangle_area(dimensions:  (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+// struct refactor
+fn struct_calc_area(reactangle: &Reactangle) -> u32{
+
+    reactangle.width * reactangle.height
+
 }
