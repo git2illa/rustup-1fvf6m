0a922825aeaf6687e1f02410124467426632fdc9
catzillaorz <catzillaorz@outlook.com>
2024-05-27 11:39:39 +0800

    feat: update if let else | enum

--- enum_with_options_match/src/main.rs
+++ enum_with_options_match/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let five = Some(5);
     let six = plus_on(five);
     let none = plus_on(None);
+
 }
 
 fn plus_on(x: Option<i32>) -> Option<i32>{
@@ -12,5 +13,12 @@ fn plus_on(x: Option<i32>) -> Option<i32>{
         Some(i) => Some(i+1)
     }
 }
-
+//fn match_some(x: Option<u8>) -> Option<u8>{
+//    match x {
+//        1 => print!("one"), 
+//       3 => print!("three"), 
+//      5 => print!("five"), 
+//     _ => print!(), 
+//}
+//}
 
