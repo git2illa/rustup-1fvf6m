41f4556772a9abdc9e5fc840e6d237b6e94880aa
catzillaorz <catzillaorz@outlook.com>
2024-05-27 15:08:52 +0800

    feat: use keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -25,7 +25,16 @@ pub mod tests {
      }
  }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant(){
+    // use 
+    hosting::add_to_waitinglist();
+    hosting::add_to_waitinglist();
+    hosting::add_to_waitinglist();
+
+
+
     // absoulute path
     crate::front_of_house::hosting::add_to_waitinglist();
 
