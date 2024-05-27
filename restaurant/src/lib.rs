d5206b02aeb83c4aa96f7df1d0b1deab1499c91d
catzillaorz <catzillaorz@outlook.com>
2024-05-27 14:56:49 +0800

    feat: mod pub struct

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -36,6 +36,20 @@ pub fn eat_at_restaurant(){
 fn serve_order(){}
 
 mod back_of_house{
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast{
+        pub fn summer(toast: &str) -> Breakfast{
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order(){
         cook_order();
         super::serve_order();
