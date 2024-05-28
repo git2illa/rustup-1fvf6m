5362fdfa3fcf8ace115be796bbfb535c92f947aa
catzillaorz <catzillaorz@outlook.com>
2024-05-28 15:00:25 +0800

    feat: impl generic struct fn

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -3,8 +3,24 @@ struct Point<T,U>{
     y: U
 }
 
+struct PointT<T>{
+    x: T,
+    y: T
+}
+
+impl<T> PointT<T>{
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     println!("----");
+    let p = PointT{
+        x: 5,
+        y: 5
+    };
+    println!("=> {} ", p.x);
     println!("----");
 }
 
