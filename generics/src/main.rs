eff608818411d1bfedbe607dfe279824cfa73b59
catzillaorz <catzillaorz@outlook.com>
2024-05-28 14:44:58 +0800

    feat: generic | struct

---
+++ generics/src/main.rs
@@ -0,0 +1,17 @@
+struct Point<T,U>{
+    x: T,
+    y: U
+}
+
+fn main() {
+    println!("----");
+    println!("----");
+}
+
+
+fn basic_struct(){
+    let integer_float  = Point{
+        x: 5,
+        y: 2.04
+    };
+}
