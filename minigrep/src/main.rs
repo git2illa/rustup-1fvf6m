33d5fe111370aa251879cefa5c477a81e4227dfd
catzillaorz <catzillaorz@outlook.com>
2024-05-29 17:42:21 +0800

    feat: refactor step1

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,31 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    let config = parse_config(&args);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something wrong when reading the file.");
+
+    println!("Text contents: \n {}", contents);
+}
+
+struct Config{
+    query: String,
+    filename: String
+}
+fn parse_config(args: &[String]) -> Config{
+
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {
+        query,
+        filename
+    }
+
+    
 }
