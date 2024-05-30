9afe0ebfcdd66a567a0dda328f59d53dcbc65810
catzillaorz <catzillaorz@outlook.com>
2024-05-30 15:01:46 +0800

    feat: impl search_case_insensitive & test case

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -7,18 +7,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duck tape.";
 
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
         );
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive,
+Pick three.
+Trust me.";
+        
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+            );
+    }
+
 }
 
 pub fn search<'a>(query: &str,  contents: &'a str) -> Vec<&'a str>{
@@ -61,3 +78,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+pub fn search_case_insensitive<'a>(query: &str,  contents: &'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query){
+            results.push(line);
+        }
+
+    }
+
+    results
+
+}
+
