325b8338d617f59cdec9f7d4245092c6c8c2b5b4
catzillaorz <catzillaorz@outlook.com>
2024-05-31 14:02:35 +0800

    feat: test closure

--- simu_expensive_calc/src/lib.rs
+++ simu_expensive_calc/src/lib.rs
@@ -1,6 +1,11 @@
+
+use std::thread;
+use std::time::Duration;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +13,83 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let x = 4;
+        let equare_to_x = |z| z == x; 
+        let y = 4;
+        assert!(equare_to_x(y));
+    }
+    #[test]
+    fn it_not_works() {
+        let x = 4;
+        fn equare_to_x(z: i32) -> bool {z == x} 
+        let y = 4;
+        assert!(equare_to_x(y));
+    }
+
+
+    #[test]
+    fn call_with_value() {
+        let mut c = Cacher::new(|a| a);
+        let c1 =  c.value(1);
+        let c2 =  c.value(2);
+        assert_eq!(c2, 2);
+    }
+
+
+}
+
+pub struct Cacher<T> 
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    pub fn new(calculation: T) -> Cacher<T>{
+        Cacher{
+            calculation,
+            value: None
+        }
+    }
+
+    pub fn value(&mut self, arg: u32) -> u32{
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+
+}
+
+pub fn simulate_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculation slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+pub fn generate_workout(intensity: u32, random_num: u32)  {
+    //let simulate_result = simulate_expensive_calculation(intensity);
+    let mut  simulate_expensive_closure = Cacher::new(|num: u32| {
+        println!("calculation slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!" , simulate_expensive_closure.value(intensity));
+        println!("Next, do {} situps!", simulate_expensive_closure.value(intensity));
+    } else {
+        if random_num == 3 {
+            println!("Take a break today!");
+        } else {
+            println!("Today run for {} minutes", simulate_expensive_closure.value(intensity));
+        }
     }
 }
--- simu_expensive_calc/src/main.rs
+++ simu_expensive_calc/src/main.rs
@@ -1,35 +1,4 @@
-use std::thread;
-use std::time::Duration;
-
-struct Cacher<T> 
-    where T: Fn(u32) -> u32
-{
-    calculation: T,
-    value: Option<u32>
-}
-
-impl<T> Cacher<T>
-    where T: Fn(u32) -> u32
-{
-    fn new(calculation: T) -> Cacher<T>{
-        Cacher{
-            calculation,
-            value: None
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32{
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-
-}
+use simu_expensive_calc::generate_workout;
 
 fn main() {
     let simulate_user_intensity = 24;
@@ -37,28 +6,3 @@ fn main() {
     generate_workout(simulate_user_intensity, simulate_random_number);
 }
 
-fn simulate_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculation slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
-fn generate_workout(intensity: u32, random_num: u32)  {
-    //let simulate_result = simulate_expensive_calculation(intensity);
-    let mut  simulate_expensive_closure = Cacher::new(|num: u32| {
-        println!("calculation slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    if intensity < 25 {
-        println!("Today, do {} pushups!" , simulate_expensive_closure.value(intensity));
-        println!("Next, do {} situps!", simulate_expensive_closure.value(intensity));
-    } else {
-        if random_num == 3 {
-            println!("Take a break today!");
-        } else {
-            println!("Today run for {} minutes", simulate_expensive_closure.value(intensity));
-        }
-    }
-}
