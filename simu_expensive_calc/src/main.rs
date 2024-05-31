2473be20b9ae92aca45549a16f8985ebf7096381
catzillaorz <catzillaorz@outlook.com>
2024-05-31 10:10:44 +0800

    feat: simu-cal step2

--- simu_expensive_calc/src/main.rs
+++ simu_expensive_calc/src/main.rs
@@ -2,11 +2,24 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    todo!();
+    generate_workout(28, 3);
 }
 
-fn basic_calc(intensity: u32) -> u32 {
+fn simulate_expensive_calculation(intensity: u32) -> u32 {
     println!("calculation slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
+}
+
+fn generate_workout(intensity: u32, random_num: u32)  {
+    let simulate_result = simulate_expensive_calculation(intensity);
+    if intensity < 25 {
+        println!("Today, do {} pushups!" , simulate_result);
+    } else {
+        if random_num == 3 {
+            println!("Take a break today!");
+        } else {
+            println!("Today run for {} minutes", simulate_result);
+        }
+    }
 }
