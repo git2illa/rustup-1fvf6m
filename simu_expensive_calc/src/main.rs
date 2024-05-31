d782664a13a0caf3ba619ead0f3d1b4baaaf2c29
catzillaorz <catzillaorz@outlook.com>
2024-05-31 11:26:22 +0800

    feat: user struct & impl struct construct & refactor simulation-calculate-fn

--- simu_expensive_calc/src/main.rs
+++ simu_expensive_calc/src/main.rs
@@ -1,6 +1,35 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T> 
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T>{
+        Cacher{
+            calculation,
+            value: None
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32{
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                v
+            }
+        }
+    }
+
+}
+
 fn main() {
     let simulate_user_intensity = 21;
     let simulate_random_number = 3;
