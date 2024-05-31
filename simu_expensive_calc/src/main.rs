da686a4d868cffa97c5445ee4de87930cbbda495
catzillaorz <catzillaorz@outlook.com>
2024-05-31 10:42:30 +0800

    feat: simu-calc step3 try closure

--- simu_expensive_calc/src/main.rs
+++ simu_expensive_calc/src/main.rs
@@ -2,7 +2,9 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    generate_workout(28, 3);
+    let simulate_user_intensity = 21;
+    let simulate_random_number = 3;
+    generate_workout(simulate_user_intensity, simulate_random_number);
 }
 
 fn simulate_expensive_calculation(intensity: u32) -> u32 {
@@ -13,6 +15,12 @@ fn simulate_expensive_calculation(intensity: u32) -> u32 {
 
 fn generate_workout(intensity: u32, random_num: u32)  {
     let simulate_result = simulate_expensive_calculation(intensity);
+    let simulate_expensive_closure = |num: u32| {
+        println!("calculation slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
     if intensity < 25 {
         println!("Today, do {} pushups!" , simulate_result);
         println!("Next, do {} situps!", simulate_result);
