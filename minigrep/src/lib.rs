9ccf49abb29c9676bcf57055c4e9c1eeca5fd3c9
catzillaorz <catzillaorz@outlook.com>
2024-05-31 16:49:17 +0800

    feat: refactor search & insensitive fn use iterator

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -40,15 +40,9 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str,  contents: &'a str) -> Vec<&'a str>{
-    let mut results = Vec::new();
-    
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    
-    results
+    contents.lines() 
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub struct Config {
@@ -98,16 +92,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 pub fn search_case_insensitive<'a>(query: &str,  contents: &'a str) -> Vec<&'a str>{
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query){
-            results.push(line);
-        }
-
-    }
-
-    results
-
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
