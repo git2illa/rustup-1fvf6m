b91f1e10c6329b3df86a9dc2ae0771ad324bd4e5
catzillaorz <catzillaorz@outlook.com>
2024-05-31 15:53:35 +0800

    feat: use iterator to refactor minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,9 +4,7 @@ use minigrep::Config;
 use minigrep::run;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else( |err| {
+    let config = Config::new(env::args()).unwrap_or_else( |err| {
         // cargo run > output.txt
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
